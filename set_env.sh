f967f94d0d579792a03202797d18ecb9528ec5fe
Duc Nam <namnd0210@gmail.com>
2025-01-05 10:31:27 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=database-2.c9azrwtsqgou.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::namnd-project3-s3 
+export AWS_REGION=us-east-1
+export AWS_PROFILE=account2
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+ 
